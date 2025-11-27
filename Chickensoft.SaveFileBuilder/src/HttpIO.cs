b005c579b41e69fccc9337e019b7c18544407c32
Mosakaas <casey.hofland@mosadex-ehealth.nl>
2025-11-27 22:50:33 +0100

    Functionally tested HttpIO
    The HttpIO is ready for the field! It now exposes the necessary headers and optional Request uris, some easy-to-use constructors and properly disposes its objects.

--- Chickensoft.SaveFileBuilder/src/HttpIO.cs
+++ Chickensoft.SaveFileBuilder/src/HttpIO.cs
@@ -4,17 +4,18 @@ using System;
 using System.IO;
 using System.Net;
 using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Threading;
 using System.Threading.Tasks;
 
-public readonly record struct HttpIOConfig(
+public readonly record struct HttpIORequestUris(
   Uri? ReadUri = null,
   Uri? WriteUri = null,
   Uri? ExistsUri = null,
   Uri? DeleteUri = null
 )
 {
-  public HttpIOConfig(
+  public HttpIORequestUris(
     string? readUri = null,
     string? writeUri = null,
     string? existsUri = null,
@@ -33,54 +34,103 @@ public class HttpIO : IAsyncIOStreamProvider, IDisposable
   private bool _isDisposed;
 
   private readonly HttpClient _httpClient;
+  private readonly bool _disposeClient;
+  private readonly HttpContent _emptyContent = new ByteArrayContent([]);
 
-  public readonly HttpIOConfig Config;
+  public HttpIORequestUris RequestUris { get; init; }
 
-  public HttpIO(Uri? baseAddress = null, HttpIOConfig config = default)
-  {
-    _httpClient = new HttpClient()
+  public HttpRequestHeaders ReadHeaders => _httpClient.DefaultRequestHeaders;
+  public HttpContentHeaders WriteHeaders => _emptyContent.Headers;
+
+  public HttpIO()
+    : this(new HttpClient())
+  { }
+
+  public HttpIO(Uri? baseAddress)
+    : this(new HttpClient()
     {
-      BaseAddress = baseAddress
-    };
+      BaseAddress = baseAddress,
+    })
+  { }
 
-    Config = config;
+  public HttpIO(Uri? baseAddress, TimeSpan timeout)
+    : this(new HttpClient()
+    {
+      BaseAddress = baseAddress,
+      Timeout = timeout
+    })
+  { }
+
+  public HttpIO(string? baseAddress)
+  : this(baseAddress is not null ? new Uri(baseAddress) : null)
+  { }
+
+  public HttpIO(string? baseAddress, TimeSpan timeout)
+    : this(baseAddress is not null ? new Uri(baseAddress) : null, timeout)
+  { }
+
+  public HttpIO(HttpClient client, bool disposeClient = true)
+  {
+    _httpClient = client;
+    _disposeClient = disposeClient;
   }
 
   public async Task<Stream> ReadAsync(CancellationToken cancellationToken = default)
   {
-    HttpResponseMessage response = default!;
+    using var response = await _httpClient.GetAsync(RequestUris.ReadUri, cancellationToken).ConfigureAwait(false);
+
     try
     {
-      response = await _httpClient.GetAsync(Config.ReadUri, cancellationToken).ConfigureAwait(false);
       response.EnsureSuccessStatusCode();
-      return await response.Content.ReadAsStreamAsync().ConfigureAwait(false);
     }
     catch (HttpRequestException)
     when (response.StatusCode is HttpStatusCode.NotFound)
     {
       return new MemoryStream();
     }
+    catch
+    {
+      throw;
+    }
+
+    await using var contentStream = await response.Content.ReadAsStreamAsync().ConfigureAwait(false);
+
+    var memoryStream = new MemoryStream();
+    await contentStream.CopyToAsync(memoryStream, cancellationToken).ConfigureAwait(false);
+    memoryStream.Position = 0;
+    return memoryStream;
   }
 
   public async Task WriteAsync(Stream stream, CancellationToken cancellationToken = default)
   {
     stream.Position = 0;
 
-    var content = new StreamContent(stream);
-    content.Headers.ContentLength = stream.Length;
+    using var content = new StreamContent(stream)
+    {
+      Headers = { ContentLength = WriteHeaders.ContentLength ?? stream.Length }
+    };
+    foreach (var header in WriteHeaders)
+    {
+      if (header.Key.Equals("Content-Length", StringComparison.OrdinalIgnoreCase))
+      {
+        continue;
+      }
 
-    await _httpClient.PostAsync(Config.WriteUri, content, cancellationToken).ConfigureAwait(false);
+      content.Headers.Add(header.Key, header.Value);
+    }
+
+    await _httpClient.PostAsync(RequestUris.WriteUri, content, cancellationToken).ConfigureAwait(false);
   }
 
   public async Task<bool> ExistsAsync(CancellationToken cancellationToken = default)
   {
-    var response = await _httpClient.GetAsync(Config.ExistsUri, HttpCompletionOption.ResponseHeadersRead, cancellationToken).ConfigureAwait(false);
+    using var response = await _httpClient.GetAsync(RequestUris.ExistsUri, HttpCompletionOption.ResponseHeadersRead, cancellationToken).ConfigureAwait(false);
     return response.IsSuccessStatusCode;
   }
 
   public async Task<bool> DeleteAsync(CancellationToken cancellationToken = default)
   {
-    var response = await _httpClient.DeleteAsync(Config.DeleteUri, cancellationToken).ConfigureAwait(false);
+    using var response = await _httpClient.DeleteAsync(RequestUris.DeleteUri, cancellationToken).ConfigureAwait(false);
     return response.IsSuccessStatusCode;
   }
 
@@ -96,7 +146,11 @@ public class HttpIO : IAsyncIOStreamProvider, IDisposable
     {
       if (disposing)
       {
-        _httpClient.Dispose();
+        if (_disposeClient)
+        {
+          _httpClient.Dispose();
+        }
+        _emptyContent.Dispose();
       }
 
       _isDisposed = true;
