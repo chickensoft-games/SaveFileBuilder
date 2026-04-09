1e36f0aaa89f0067c35fd551f7fd39e1a1c9876d
Mosakaas <casey.hofland@mosadex-ehealth.nl>
2026-04-10 01:48:51 +0200

    XmlStreamSerializer aot support

--- Chickensoft.SaveFileBuilder/src/Serialization/XmlStreamSerializer.cs
+++ Chickensoft.SaveFileBuilder/src/Serialization/XmlStreamSerializer.cs
@@ -5,10 +5,17 @@ using System.IO;
 using System.Xml;
 using System.Xml.Serialization;
 
+#if NET8_0_OR_GREATER
+using System.Diagnostics.CodeAnalysis;
+#endif
+
 /// <summary>Provides functionality to serialize from- and deserialize to objects or value types using the <see cref="XmlSerializer"/>.</summary>
 /// <remarks>
 /// <see cref="XmlStreamSerializer"/> does not provide options for specifying SOAP serialization. SOAP is an application communication protocol, not meant for saving and loading scenarios such as the <see cref="XmlStreamSerializer"/> provides.
 /// </remarks>
+#if NET8_0_OR_GREATER
+[RequiresUnreferencedCode($"{nameof(XmlStreamSerializer)} requires unreferenced code and can break functionality when trimming application code.")]
+#endif
 public class XmlStreamSerializer : IStreamSerializer
 {
   /// <summary>Settings for deserializing from xml.</summary>
@@ -21,10 +28,10 @@ public class XmlStreamSerializer : IStreamSerializer
   public XmlSerializerNamespaces SerializerNamespaces { get; init; } = new();
 
   /// <summary>The context information required when deserializing from xml.</summary>
-  public XmlParserContext? ParserContext { get; init; } = null;
+  public XmlParserContext? ParserContext { get; init; }
 
   /// <summary>Contains events that are invoked during different stages of the deserialization process.</summary>
-  public XmlDeserializationEvents DeserializationEvents { get; init; } = default;
+  public XmlDeserializationEvents DeserializationEvents { get; init; }
 
   /// <inheritdoc />
   public void Serialize(Stream stream, object? value, Type inputType)
